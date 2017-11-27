acb0b8b4e9c77b81bead92e5034fa9c80af3b5ab
manolescusebastian <sebastian.manolescu@gmail.com>
2017-11-27 17:46:07 +0200

    problema 1 rezolvata_numere reale/ numere intregi

---
+++ src/main/java/com/codegile/university/ioana/week1/day1/problem1/Main.java
@@ -0,0 +1,51 @@
+package com.codegile.university.ioana.week1.day1.problem1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+
+        String line = "";
+
+        File file = new File("/Users/manolescusebastian/IdeaProjects/Ex_codegile/src/Numere.txt");
+        Scanner scanner = new Scanner(file);
+
+        try{
+            while (scanner.hasNextLine()){
+                line = scanner.nextLine();
+            }
+
+            convertValues(line);
+
+        }finally {
+            scanner.close();
+        }
+
+
+    }
+
+
+    private static void convertValues(String line){
+
+        int doubleCount = 0;
+        int integerCount = 0;
+
+        String[] splitNumbersByComma = line.split(",");
+
+        for(String number : splitNumbersByComma){
+            if(number.contains(".")){
+                doubleCount++;
+            }else{
+                integerCount++;
+            }
+        }
+
+        System.out.println("Double count value:" + doubleCount);
+        System.out.println("Integer count value:" + integerCount);
+    }
+
+}
